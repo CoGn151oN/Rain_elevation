771159efaa27c11ccfe77253871168724045f8f9
CoGn151oN <121181789+CoGn151oN@users.noreply.github.com>
2023-06-18 15:21:18 +0200

    preprocessing

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from graph import Graph
 
 if __name__ == '__main__':
-    elevation = [[25, 37, 45, 49],
-                 [51, 12, 34, 25],
-                 [94, 83, 27, 29]]
-    
+    stdin = """3 4
+    25 37 45 49
+    51 12 34 25
+    94 83 27 29
+    10000"""
+
+    # preprocess
+    row, col = [int(n) for n in stdin.split()[:2]]
+    elevation = [list(map(int, i.split())) for i in stdin.split("\n")[1:-1]]
+    precipitation = float(stdin.split()[-1])
+
     Graph.plot(elevation)
+
 
 
 
